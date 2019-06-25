280940d181e709df4931fbe84eb4c3788ebd0e93
nishanth <nishanth.infra@gmail.com>
2019-06-24 22:03:25 -0400

    commented the error

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
 /**
- @param name of the person
- @return greetings message
-*
+* @param  someone of the person
+* @return greetings message
+
 */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
